530538653588032c0b88e967e2c84588af089adb
ecrz00 <erick.gomez.crz@gmail.com>
2025-12-18 14:05:35 -0600

    updating header

--- 46.Permutations.py
+++ 46.Permutations.py
@@ -1,3 +1,18 @@
 '''
-
+Approach: We are going to use backtrack to solve this problem. Since all the numbers in nums are unique, we can check if num is already 
+in the array. 
 '''
+def permute(nums: list[int]) -> list[list[int]]:
+    n = len(nums)
+    res, sol = [], []
+    def backtrack():
+        if len(sol) == n:
+            res.append(sol[:])
+            return
+        for num in nums:
+            if num not in sol:
+                sol.append(num)
+                backtrack()
+                sol.pop()
+    backtrack()
+    return res
